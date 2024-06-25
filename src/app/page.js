aabdbfec656600b201dbd7ac9c7cf6d28d171ee5
Manish7113 <kumawatji7113@gmail.com>
2024-06-25 12:44:34 +0530

    updates

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,24 @@
-import Image from "next/image";
-
+'use client'
+import About from "./@core/component/about";
+import Contact from "./@core/component/contact";
+import Experience from "./@core/component/experience";
+import Footer from "./@core/component/footer";
+import Navbar from "./@core/component/navbar";
+import Project from "./@core/component/project";
+import Skill from "./@core/component/skills";
 
 export default function Home() {
   return (
-   <p>mail page</p>
+  <>
+    <Navbar></Navbar>
+    <About></About>
+    <Skill></Skill>
+    <Project></Project>
+    <Experience></Experience>
+    <Contact></Contact>
+    <Footer></Footer>
+    
+    
+    </>
   );
 }
